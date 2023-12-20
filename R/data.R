f98e245d33d074a6952fd954864993b55676b14e
sinast3000 <streicher@kof.ethz.ch>
2023-12-20 23:52:09 +0100

    fix bug in subgroup growth weights computation

--- R/data.R
+++ R/data.R
@@ -61,7 +61,7 @@ prepate_data <- function(
   extend_weights = FALSE
 ) {
 
-  . <- NULL
+  . <- y <- NULL
   
   if (is.null(tsl_p) & is.null(tsl_n)) stop("Either 'tsl_p' or 'tsl_n' must be supplied.")
   
@@ -131,6 +131,7 @@ prepate_data <- function(
       stats::lag(tsl[[x]] / tsl[[settings$subgroup1$load_name]], -1)
     }) %>%
       do.call(cbind, .)
+    tsl_wgrowth[["subgroup1"]][, 1] <- -1
     colnames(tsl_wgrowth[["subgroup1"]]) <- idx
     
     tsl_wlevel[["subgroup1"]] <- ts(
@@ -287,56 +288,3 @@ compute_weights <- function(tsl_r, tsl_n, idx, pos, neg, weight_type = "growth")
   
   return(tsl_w)
 }
-
-#' 
-#' # ------------------------------------------------------------------------------
-#' 
-#' #' Computes nominal weights from sub sector data
-#' #'
-#' #' @param tsl_r time series list with real level series
-#' #' @param tsl_n time series list with nominal level series
-#' #' @param idx index for aggregate
-#' #' @param pos sectors to be added
-#' #' @param neg sectors to be substracted 
-#' #' 
-#' #' @return A time series list with weights.
-#' #' 
-#' #' @importFrom stats window<-
-#' #' @keywords internal
-#' compute_weights <- function(tsl_r, tsl_n, idx, pos, neg) {
-#'   
-#'   # compute prices  
-#'   tsl_p <- lapply(names(tsl_r), function(x) {
-#'     tsl_n[[x]] / tsl_r[[x]] * 100
-#'   })
-#'   names(tsl_p) <- names(tsl_r)
-#'   # # prolong price series if necessary
-#'   tsl_p <- lapply(names(tsl_p), function(x) {
-#'     end_date_r <- last(time(zoo::na.trim(tsl_r[[x]])))
-#'     end_date_p <- last(time(zoo::na.trim(tsl_p[[x]])))
-#'     y <- tsl_p[[x]]
-#'     if (end_date_r > end_date_p) {
-#'       window(y, start = end_date_p, end = end_date_r, extend = TRUE) <- as.numeric(last(zoo::na.trim(y)))
-#'     }
-#'     y
-#'   })
-#'   
-#'   # previous year prices
-#'   tsl_p <- lapply(tsl_p, function(x) {
-#'     y <- stats::lag(ta(x, to = "annual"), -1)
-#'     td(y ~ -1, method = "uniform")$values
-#'   })
-#'   names(tsl_p) <- names(tsl_r)
-#'   
-#'   # compute weights for prices and real level series
-#'   w_p <- do.call(cbind, tsl_p) / tsl_p[[idx]]
-#'   tsl_w <- as.list(w_p)
-#'   names(tsl_w) <- names(tsl_p)
-#'   
-#'   # change sign of negative contributions and aggregate
-#'   tsl_w[idx] <- lapply(tsl_w[idx], function(x) -x)
-#'   tsl_w[neg] <- lapply(tsl_w[neg], function(x) -x)
-#'   
-#'   return(tsl_w)
-#' }
-#' 
